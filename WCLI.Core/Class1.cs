de4a83de7865b43f982062e470b9d9922cbd187e
Maloney, John <maloney1@gmail.com>
2021-11-30 11:02:51 -0700

    Moving to .Net 5 Framework

---
+++ WCLI.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace WCLI.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ WCLI.Core/Contracts/IEnvironmentRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCLI.Core.Contracts
+{
+    public interface IEnvironmentRepository
+    {
+        /// <summary>
+        /// Stores the default context name for the auth credentials
+        /// </summary>
+        string DefaultContext { get; set; }
+
+        /// <summary>
+        /// This is the containing folder for the *settings.json files.
+        /// </summary>
+        string RootDirectory { get; set; }
+
+        /// <summary>
+        /// Individual Authorization contexts related to an individuals account.
+        /// </summary>
+        Dictionary<string, IAuthContext> AuthorizationContexts { get; }
+
+        /// <summary>
+        /// Environmental variables related to each specific servers urls. 
+        /// </summary>
+        IEnvironmentalInfo this[string environment] { get; }
+
+        /// <summary>
+        /// Returns the available environment names.
+        /// </summary>
+        IEnumerable<string> AvailableEnvironments { get; }
+
+        IAuthContext GetDefaultAuthContext();
+
+        void AddEnvironment(string environmentName, IEnvironmentalInfo environment);
+    }
+}
+}
